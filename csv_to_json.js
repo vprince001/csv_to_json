20888a6447c740b5981bd0b691a0a80068ad5bb4
Prince Verma <16836599+vprince001@users.noreply.github.com>
2020-01-27 18:31:36 +0530

    fix error

--- csv_to_json.js
+++ csv_to_json.js
@@ -6,7 +6,7 @@ const main = function (params) {
     const data = getData(params, fs);
     const headers = params.headers ? params.headers : extractValues(data[0].split(ES), params.separator);
     const dataInArray = params.hasHeader ? data.slice(1) : data;
-    getFinalResult(dataInArray, headers, params.separator);
+    return getFinalResult(dataInArray, headers, params.separator);
 };
 
 module.exports = main;
